8cc337a2729c77523680a94408138c254cc73ca5
TechGyaanLabs <techgyaanlabs@gmail.com>
2024-09-18 08:02:40 +0530

    Added new class to print pattern

---
+++ corejava/src/main/java/com/careerit/cj/day5/NestedForLoops.java
@@ -0,0 +1,17 @@
+package com.careerit.cj.day5;
+
+public class NestedForLoops {
+
+
+    /*
+       if n value 5
+       A
+       A B
+       A B C
+       A B C D
+       A B C D E
+
+     */
+    public static void printPattern(int n){}
+
+}
